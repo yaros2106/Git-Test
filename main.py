bc3ff7e66e1c257a582b02a657b339bc28f193d8
Yaroslav <iaroslav.bierioza@gmail.com>
2025-07-25 14:21:51 +0300

    new rebase commit

--- main.py
+++ main.py
@@ -3,3 +3,24 @@ def printer():
     print("hello world")
     print("added from feature branch")
     print("something else")
+
+# main comment
+def main():
+    printer()
+
+def one():
+    pass
+
+def two():
+    pass
+
+def three():
+    pass
+
+def four():
+    pass
+
+
+if __name__ == '__main__':
+    # last comment
+    main()
