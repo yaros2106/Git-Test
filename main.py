22b87daf25cbacafb52f85fa64f37e5a09f8950a
Yaroslav <iaroslav.bierioza@gmail.com>
2025-07-26 14:15:09 +0300

    some work

--- main.py
+++ main.py
@@ -1,26 +1,8 @@
-# some comment
 def printer():
     print("hello world")
     print("added from feature branch")
     print("something else")
 
-# main comment
-def main():
-    printer()
-
-def one():
-    pass
-
-def two():
-    pass
-
-def three():
-    pass
-
-def four():
-    pass
-
 
 if __name__ == '__main__':
-    # last comment
-    main()
+    printer()
